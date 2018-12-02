b86e5e346d7c60f0a8157727197bebdffa48acb5
CPT <chandraprakashtiwari247@gmail.com>
2018-12-02 14:31:31 +0530

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,10 @@ var socket = io();
 socket.on('connect', ()=>{
     console.log("Connected to server");
 
-    socket.emit("createMessage",{
-        to : "Andrew",
-        text : "How are you?"
-    });
+//    socket.emit("createMessage",{
+//        to : "Andrew",
+//        text : "How are you?"
+//    });
 
 });
 socket.on('disconnect', ()=>{
--- server/server.js
+++ server/server.js
@@ -14,12 +14,32 @@ console.log(finalPath);
 io.on('connection', (socket)=>{
     console.log("New user connected");
     
+    socket.emit("newMessage",{
+        from : "Admin",
+        text: "Welcome to Chat App"
+    });
+    
+    socket.broadcast.emit("newMessage",{
+        from : "Admin",
+        text: "New User joined!"
+    });
+    
     socket.on('disconnect', ()=>{
        console.log("User disconnected"); 
     });
     
     socket.on("createMessage", (message)=>{
-        console.log(message);
+        //console.log(message);
+        io.emit("newMessage", {
+            from : message.to,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+//        socket.broadcast.emit("newMessage", {
+//            from: message.to,
+//            text : message.text,
+//            createdAt : new Date().getTime()
+//        });
     });
     
     socket.emit("newMessage",{
